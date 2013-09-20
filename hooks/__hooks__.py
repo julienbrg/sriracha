7523e2025c71287e43dd402500ef33bd03fba4b8
uberj <uberj@onid.orst.edu>
2013-09-20 15:30:30 -0700

    fix footer

--- hooks/__hooks__.py
+++ hooks/__hooks__.py
@@ -1,11 +1,13 @@
 import thumbnail
+from hooks import compile_sass
 from gallery import Gallery
 
 
 gallery = Gallery()
 
 hooks = {
-    'site.start': [thumbnail.create_thumbnails],
+    #'site.start': [thumbnail.create_thumbnails],
     'page.meta.post': [gallery.get_images],
     'page.template.pre': [gallery.get_albums, gallery.set_images],
+    'site.output.post': [compile_sass],
 }
