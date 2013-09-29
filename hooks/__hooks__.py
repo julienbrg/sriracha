f21f76e6fe09228249367a41db29e9f046a64daf
Kevin Ngo <ktngo09@gmail.com>
2013-09-29 22:18:38 +0000

    Revert "fix footer"
    This reverts commit 7523e2025c71287e43dd402500ef33bd03fba4b8.

--- hooks/__hooks__.py
+++ hooks/__hooks__.py
@@ -1,13 +1,11 @@
 import thumbnail
-from hooks import compile_sass
 from gallery import Gallery
 
 
 gallery = Gallery()
 
 hooks = {
-    #'site.start': [thumbnail.create_thumbnails],
+    'site.start': [thumbnail.create_thumbnails],
     'page.meta.post': [gallery.get_images],
     'page.template.pre': [gallery.get_albums, gallery.set_images],
-    'site.output.post': [compile_sass],
 }
